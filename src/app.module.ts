1a98e024d148c81d5cfac4f660048f9ce772738d
TalhaKhan1122 <tk42100678@gmail.com>
2026-05-06 13:06:29 +0500

    Fix:Refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,27 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 import { TasksModule } from "./tasks/tasks.module";
 import { UsersModule } from "./users/users.module";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 
-const mongoUri =
-  process.env.MONGO_URI ||
-  "mongodb+srv://tk42100678_db_user:FML3Y313Cez5HL8G@cluster0.4l0aakx.mongodb.net/";
-
+const mongoUri = process.env.MONGO_URI;
 @Module({
-  imports: [MongooseModule.forRoot(mongoUri), TasksModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => {
+        const uri = config.get<string>("MONGO_URI");
+
+        if (!uri) {
+          throw new Error("MONGO_URI is not defined in .env");
+        }
+
+        return { uri };
+      },
+    }),
+    TasksModule,
+    UsersModule,
+  ],
 })
 export class AppModule {}
